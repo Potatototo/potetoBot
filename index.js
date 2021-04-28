7a68133cc86978f8c0c548a8123417bce8864ece
Sebastian Vöge <stu202417@mail.uni-kiel.de>
2021-04-28 10:56:45 +0200

    comments & refactoring

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const ytdl = require('ytdl-core');
 const fs = require('fs');
 
 const client = new Discord.Client();
+
+// load commands
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -15,34 +17,39 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
+// setup state holder with queue, volume, voice channel, text channel
+// TODO
+
+// Connection updates
 client.once('ready', () => {
     console.log('Ready!');
-   });
-   client.once('reconnecting', () => {
+});
+client.once('reconnecting', () => {
     console.log('Reconnecting!');
-   });
-   client.once('disconnect', () => {
+});
+client.once('disconnect', () => {
     console.log('Disconnect!');
-   });
+});
 
+// Command handling
 client.on('message', async message => {
+    // no prefix
     if (message.author.bot || !message.content.startsWith(prefix)) return;
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
+    // not a command
     if (!client.commands.has(commandName)) return;
     const command = client.commands.get(commandName);
 
-    try {
-	    command.execute(message, args);
+    // command execution
+	try {
+        command.execute(message, args);   
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
     }
 })
-
-
-
 
 client.login(token);
