7c3b23a17d70e016710e22e2a47459d1a1e9d328
Sebastian Vöge <stu202417@mail.uni-kiel.de>
2021-09-17 21:00:30 +0200

    np embed reformat

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -6,28 +6,18 @@ module.exports = {
 	description: 'Currently playing song',
 	execute(message, args, queueHolder) {
 		const vc = message.member.voice.channel;
+		const e = new MessageEmbed()
+			.setColor('#E6722E')
+			.setAuthor('potetoBot', 'https://i.imgur.com/8HzsYp9.png')
 		if (!vc) {
-			const e = new MessageEmbed()
-				.setColor('#E6722E')
-				.setAuthor('potetoBot', 'https://i.imgur.com/8HzsYp9.png')
-				.addField('\u200b', 'You need to be in a voice channel for this!', false)
-				.setTimestamp()
-			message.channel.send({ embeds: [e] });
+			e.addField('\u200b', 'You need to be in a voice channel for this!', false)
 		}
 		if (queueHolder.currentSong) {
-			const e = new MessageEmbed()
-				.setColor('#E6722E')
-				.setAuthor('potetoBot', 'https://i.imgur.com/8HzsYp9.png')
-				.addField('Currently playing', queueHolder.currentSong, false)
-				.setTimestamp()
-			message.channel.send({ embeds: [e] });
+			e.addField('Currently playing', queueHolder.currentSong, false);
 		} else {
-			const e = new MessageEmbed()
-				.setColor('#E6722E')
-				.setAuthor('potetoBot', 'https://i.imgur.com/8HzsYp9.png')
-				.addField('\u200b', 'Nothing is playing at the moment.', false)
-				.setTimestamp()
-			message.channel.send({ embeds: [e] });
+			e.addField('\u200b', 'Nothing is playing at the moment.', false);
 		}
+		e.setTimestamp();
+		message.channel.send({ embeds: [e] });
 	},
 };
