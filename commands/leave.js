b56e4f58ce1949956cac5c3d65170de504a9e6f5
Sebastian Vöge <sebastian.voege@live.de>
2021-09-22 19:25:50 +0200

    reset queueHolder on leave

--- commands/leave.js
+++ commands/leave.js
@@ -5,6 +5,11 @@ module.exports = {
 	async execute(message, args, queueHolder) {
 		try {
 			await queueHolder.subscription.connection.destroy();
+			queueHolder.textChannel = null;
+    		queueHolder.subscription = null;
+    		queueHolder.songs = [];
+    		queueHolder.currentSong = null;
+    		queueHolder.volume = 0.2;
 		} catch (err) {
 			console.log(err);
 		}
