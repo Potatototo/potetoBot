5e795e4eb2b4938e9c768de6c0d1247744dfb20e
Sebastian Vöge <stu202417@mail.uni-kiel.de>
2022-01-02 17:18:57 +0100

    Add progress bar to nowplaying

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -16,12 +16,40 @@ module.exports = {
 		}
 		if (queueHolder.currentSong) {
 			songTitle = `${queueHolder.currentSong.title} - ${queueHolder.currentSong.ownerChannelName}`;
-			pbDuration = Math.floor(queueHolder.subscription.player.state.playbackDuration / 1000);
+			songLength = parseInt(queueHolder.currentSong.lengthSeconds)
+			pbDuration = ~~(queueHolder.subscription.player.state.playbackDuration / 1000);
+			durationString = generateDurationDisplay(songTitle, songLength, pbDuration)
 
-			e.addField('Currently playing', songTitle/* + "\n" + pbDuration*/, false);
+			e.addField('Currently playing', songTitle + "\n" + durationString, false);
 		} else {
 			e.addField('\u200b', 'Nothing is playing at the moment.', false);
 		}
 		message.channel.send({ embeds: [e] });
 	},
 };
+
+function fancyTimeFormat(duration) {   
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function generateDurationDisplay(songTitle, songLength, pbDuration) {
+	const barCount = 25
+	durationString = `${fancyTimeFormat(pbDuration)}\/${fancyTimeFormat(songLength)}`
+	const filled = ~~(pbDuration / songLength * barCount)
+	let bars = ""
+	for (let i = 0; i < barCount; i++) {
+		bars += i < filled ? "█" : "░"
+	}
+	return `${bars} ${durationString}`
+}
