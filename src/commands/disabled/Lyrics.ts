36d9c1c8d8c290ab40dc1d35e96ff5a275ba27fc
Sebastian Vöge <sebastian.voege@live.de>
2022-09-20 18:44:02 +0200

    Disable Lyrics command

--- src/commands/disabled/Lyrics.ts
+++ src/commands/disabled/Lyrics.ts
@@ -1,8 +1,8 @@
-import config from "../config.json";
+import config from "../../config.json";
 import { Message } from "discord.js";
 import Genius from "genius-lyrics";
-import { Command } from "../types/Command";
-import { CommandCategory } from "../types/CommandCategory";
+import { Command } from "../../types/Command";
+import { CommandCategory } from "../../types/CommandCategory";
 
 const Client = new Genius.Client(config.genius);
 
